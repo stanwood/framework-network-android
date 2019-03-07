0b6b88fedeee8751980e5bd409c551d72de4d179
Sven Bendel <ubuntudroid@gmail.com>
2019-03-07 12:47:33 +0100

    SFW-274 Update copyright

--- network/src/main/java/io/stanwood/framework/network/auth/Authenticator.kt
+++ network/src/main/java/io/stanwood/framework/network/auth/Authenticator.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 stanwood GmbH
+ * Copyright (c) 2019 stanwood GmbH
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
