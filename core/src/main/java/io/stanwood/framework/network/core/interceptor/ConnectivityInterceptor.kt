aadb74200536fca6efe8eaf3f664263bb98c82ad
Sven Bendel <ubuntudroid@gmail.com>
2019-03-22 15:27:28 +0100

    SFW-264 Split up library into three modules: core, cache and auth

--- core/src/main/java/io/stanwood/framework/network/core/interceptor/ConnectivityInterceptor.kt
+++ core/src/main/java/io/stanwood/framework/network/core/interceptor/ConnectivityInterceptor.kt
@@ -19,35 +19,24 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+package io.stanwood.framework.network.core.interceptor
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import android.app.Application
+import io.stanwood.framework.network.core.util.ConnectionState
+import io.stanwood.framework.network.core.util.NoConnectivityException
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.io.IOException
 
-buildscript {
-    ext.kotlin_version = '1.3.21'
-    ext.okhttp_version = '3.13.1'
-    repositories {
-        google()
-        jcenter()
-    }
-    dependencies {
-        classpath 'com.android.tools.build:gradle:3.3.2'
-        classpath 'com.github.dcendents:android-maven-gradle-plugin:2.1'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        jcenter()
-        maven { url "https://kotlin.bintray.com/kotlinx" }
-        maven { url "https://jitpack.io" }
-    }
-}
-
-task clean(type: Delete) {
-    delete rootProject.buildDir
+class ConnectivityInterceptor(context: Application) : Interceptor {
+    private val connectionState = ConnectionState(context)
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response =
+        connectionState.isConnected.let {
+            if (it) {
+                chain.proceed(chain.request())
+            } else {
+                throw NoConnectivityException()
+            }
+        }
 }
--- core/src/main/java/io/stanwood/framework/network/core/util/ConnectionState.kt
+++ core/src/main/java/io/stanwood/framework/network/core/util/ConnectionState.kt
@@ -20,34 +20,19 @@
  * SOFTWARE.
  */
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+package io.stanwood.framework.network.core.util
 
-buildscript {
-    ext.kotlin_version = '1.3.21'
-    ext.okhttp_version = '3.13.1'
-    repositories {
-        google()
-        jcenter()
-    }
-    dependencies {
-        classpath 'com.android.tools.build:gradle:3.3.2'
-        classpath 'com.github.dcendents:android-maven-gradle-plugin:2.1'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+import android.content.Context
+import android.net.ConnectivityManager
 
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
-}
+class ConnectionState(applicationContext: Context) {
 
-allprojects {
-    repositories {
-        google()
-        jcenter()
-        maven { url "https://kotlin.bintray.com/kotlinx" }
-        maven { url "https://jitpack.io" }
-    }
-}
+    private val connectivityManager: ConnectivityManager =
+        applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+    val isConnected: Boolean
+        get() =
+            connectivityManager.activeNetworkInfo.let {
+                it != null && it.isConnected
+            }
 }
